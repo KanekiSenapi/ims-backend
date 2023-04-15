11e908a5ae9c365a171bc4eecf1cf3fd2994f9a5
Michal Oprych <michal.marcin.oprych@gmail.com>
2023-04-15 18:12:32 +0200

    IMS-2 Prepare Open API Documentation for Customer Domain

--- src/main/java/pl/aogiri/ims/common/controller/BaseCrudController.java
+++ src/main/java/pl/aogiri/ims/common/controller/BaseCrudController.java
@@ -1,24 +1,66 @@
 package pl.aogiri.ims.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Base CRUD Operations")
 public interface BaseCrudController<DTO> {
 
     @PostMapping
-    DTO create(final DTO dto);
+    @Operation(
+            summary = "Create a new resource",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created", content = @Content(schema = @Schema(implementation = Object.class)))
+            }
+    )
+    DTO create(@RequestBody final DTO dto);
 
     @GetMapping(path = "{id}")
-    DTO getById(@PathVariable final UUID id);
+    @Operation(
+            summary = "Get a resource by ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource found", content = @Content(schema = @Schema(implementation = Object.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found")
+            }
+    )
+    DTO getById(@PathVariable @Parameter(description = "Resource ID") final UUID id);
 
     @GetMapping
+    @Operation(
+            summary = "Get all resources",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resources found", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Object.class))))
+            }
+    )
     List<DTO> getAll();
 
     @PutMapping(path = "{id}")
-    DTO update(@PathVariable final UUID id, @RequestBody final DTO dto);
+    @Operation(
+            summary = "Update a resource by ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource updated", content = @Content(schema = @Schema(implementation = Object.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found")
+            }
+    )
+    DTO update(@PathVariable @Parameter(description = "Resource ID") final UUID id, @RequestBody final DTO dto);
 
     @DeleteMapping(path = "{id}")
-    void delete(@PathVariable final UUID id);
+    @Operation(
+            summary = "Delete a resource by ID",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Resource deleted"),
+                    @ApiResponse(responseCode = "404", description = "Resource not found")
+            }
+    )
+    void delete(@PathVariable @Parameter(description = "Resource ID") final UUID id);
+
 }
--- src/main/java/pl/aogiri/ims/customer/presentation/controller/CustomerController.java
+++ src/main/java/pl/aogiri/ims/customer/presentation/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package pl.aogiri.ims.customer.presentation.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.aogiri.ims.common.controller.BaseCrudController;
 import pl.aogiri.ims.customer.presentation.dto.CustomerDTO;
 
+@Tag(name = "Customers", description = "Operations related to customers")
 @RequestMapping("/customer")
 public interface CustomerController extends BaseCrudController<CustomerDTO> {
 }
